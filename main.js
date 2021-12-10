3c162329978d95148e300d46589b15c958183e14
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2021-12-10 11:02:48 +0700

    Challenge END

--- main.js
+++ main.js
@@ -293,6 +293,15 @@ function main(){
         if (event.keyCode == 32) {
             presSpace = !presSpace;
         }
+        /*
+        if (event.keyCode == 65) 
+        if (event.keyCode == 68) 
+        if (event.keyCode == 38) 
+        if (event.keyCode == 40) 
+        if (event.keyCode == 37) 
+        if (event.keyCode == 39) 
+        */
+
     }
     document.addEventListener("keydown", onKeyPressed);
    
