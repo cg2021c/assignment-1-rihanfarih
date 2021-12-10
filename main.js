47990b158f13b68e7109ca3dd0a346045fae5d0f
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2021-12-10 11:04:11 +0700

    Challenge END

--- main.js
+++ main.js
@@ -295,10 +295,23 @@ function main(){
         }
         /*
         if (event.keyCode == 65) 
+        {
+
+        }
         if (event.keyCode == 68) 
+        {
+
+        }
         if (event.keyCode == 38) 
-        if (event.keyCode == 40) 
-        if (event.keyCode == 37) 
+        {
+
+        }
+        if (event.keyCode == 40) {
+
+        }
+        if (event.keyCode == 37) {
+            
+        }
         if (event.keyCode == 39) 
         */
 
